81105fcc3057f9a55f7ea5074b573fd5fd0fc5d4
Shamila <sremtoula@simplon.co>
2019-09-17 14:46:54 +0200

    Updates

---
+++ precache-manifest.43b05d000a463439f7cc94f955f5ff5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda04be9099cc1cd033e3308cf3e4c6b",
+    "url": "/PokeAPI/index.html"
+  },
+  {
+    "revision": "25e3c7566fac146ec2b3",
+    "url": "/PokeAPI/static/css/main.7e73f9fe.chunk.css"
+  },
+  {
+    "revision": "693e24fa48aa12d5d30c",
+    "url": "/PokeAPI/static/js/2.68838afc.chunk.js"
+  },
+  {
+    "revision": "25e3c7566fac146ec2b3",
+    "url": "/PokeAPI/static/js/main.2b260436.chunk.js"
+  },
+  {
+    "revision": "733f7b8656d48d800a33",
+    "url": "/PokeAPI/static/js/runtime~main.c6445bd0.js"
+  },
+  {
+    "revision": "8f80a92d7ba78cb0e5a3e30927022f31",
+    "url": "/PokeAPI/static/media/Pokemon_logo.8f80a92d.png"
+  }
+]);
